bcd043fbe2c912e6abc808516458fb4ca5b61263
Elendow <elendow@gmail.com>
2015-02-28 21:36:57 +0100

    Intento de Yo

--- app/src/main/java/com/elendow/kairoshacks2015/MainActivity.java
+++ app/src/main/java/com/elendow/kairoshacks2015/MainActivity.java
@@ -1,19 +1,31 @@
 package com.elendow.kairoshacks2015;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
+
+    Button btnYo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        btnYo = (Button)findViewById(R.id.btnYo);
 
+        btnYo.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                new DoYo().doInBackground("ALICENARA");
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
