5f59e46325f2239ca404f067ae61e3f03ff7d613
Alice <alicenara165@gmail.com>
2015-03-01 10:31:01 +0100

    Arraylol

--- PHP_files/getPonyPlayers.php
+++ PHP_files/getPonyPlayers.php
@@ -4,7 +4,7 @@
     mono_query("SELECT username FROM KHGame ORDER BY username",$people_playing,0);
     
     for($i=0;$i<count($people_playing);$i++){
-        echo ($people_playing[$i]);
+        echo ($people_playing[$i]['username']);
         if($i<count($people_playing)) echo(";");
     }
 ?>
